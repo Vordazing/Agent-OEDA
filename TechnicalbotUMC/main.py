6ea5a815e8652021186a746e17783e9478598d4e
VorDaz <frankiniiitain@gmail.com>
2023-12-11 11:05:18 +0300

    Add Technical bot

--- TechnicalbotUMC/main.py
+++ TechnicalbotUMC/main.py
@@ -10,13 +10,14 @@ API_TOKEN = config.token
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
 
 
 async def main():
-    dp = Dispatcher()
     dp.message.register(menu.on_start_command, Command(commands='start'))
     dp.message.register(menu.choice_object, F.text == "Начать прослушивание микротика на объекте🛂")
     dp.callback_query.register(microtik.work, lambda c: c.data.startswith('_'))
+    dp.callback_query.register(microtik.stop, F.data == 'stop')
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
--- TechnicalbotUMC/object/microtik.py
+++ TechnicalbotUMC/object/microtik.py
@@ -1,7 +1,12 @@
+import json
+import os
+import time
+
 import routeros_api
 from aiogram import types
 from object.list import mikrotik_data
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from main import dp
 
 
 def fetch_address_lists(host, username, password):
@@ -19,15 +24,23 @@ def time_mikrotik(host, username, password):
     return mikrotik_time
 
 
-async def work(callback: types.CallbackQuery):
-    button_value = callback.data.replace('_', '')
-    mikrotik_info = mikrotik_data.get(button_value)
-    data_time = time_mikrotik(mikrotik_info['HOST'], mikrotik_info['USERNAME'], mikrotik_info['PASSWORD'])
-    await callback.message.edit_text(text=f"Дата на микроте {button_value}: {data_time['date']}\n"
-                                          f"Время на микроте {button_value}: {data_time['time']}")
+async def send_message(callback, mikrotik_info, data_time):
     sent_address = None
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(text="Остановить",callback_data="stop"))
+    user_file_path = 'body/user.json'
+    start_time = time.time()
+    message = await callback.message.answer(reply_markup=builder.as_markup(), text='👾Запускаю...')
+    dots_count = 0
+
     while True:
         data_list = fetch_address_lists(mikrotik_info['HOST'], mikrotik_info['USERNAME'], mikrotik_info['PASSWORD'])
+        if os.path.exists(user_file_path):
+            with open(user_file_path, 'r') as json_file:
+                user_data = json.load(json_file)
+            if user_data.get("user_id") == callback.from_user.id and not user_data.get("flag", False):
+                break
+
         for entry in data_list:
             date_str, time_str = entry['creation-time'].split()
             if date_str == data_time['date'] and data_time['time'] <= time_str:
@@ -35,5 +48,56 @@ async def work(callback: types.CallbackQuery):
                     print(entry['address'], entry['creation-time'])
                     await callback.message.answer(text=f"{entry['address']}")
                     sent_address = entry['address']
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 60:
+            break
+
+        dots = "." * dots_count
+        text_new = f'👾В работе {dots} 👾'
+        await message.edit_text(text=text_new, reply_markup=builder.as_markup())
+        dots_count = (dots_count + 1) % 4
+
+
+async def work(callback: types.CallbackQuery):
+    button_value = callback.data.replace('_', '')
+    mikrotik_info = mikrotik_data.get(button_value)
+    data_time = time_mikrotik(mikrotik_info['HOST'], mikrotik_info['USERNAME'], mikrotik_info['PASSWORD'])
+    user_file_path = 'body/user.json'
+
+    if os.path.exists(user_file_path):
+        with open(user_file_path, 'r') as json_file:
+            user_data = json.load(json_file)
+
+        if user_data.get("user_id") == callback.from_user.id:
+            user_data["flag"] = True
+        else:
+            user_data = {
+                "user_id": callback.from_user.id,
+                "flag": True
+            }
+    else:
+        user_data = {
+            "user_id": callback.from_user.id,
+            "flag": True
+        }
+    with open(user_file_path, 'w') as json_file:
+        json.dump(user_data, json_file)
+
+    await callback.message.edit_text(text=f"Дата на микроте {button_value}: {data_time['date']}\n"
+                                          f"Время на микроте {button_value}: {data_time['time']}")
+    await send_message(mikrotik_info=mikrotik_info, data_time=data_time, callback=callback)
+
+
+async def stop(callback: types.CallbackQuery):
+    user_file_path = 'body/user.json'
+    with open(user_file_path, 'r') as json_file:
+        user_data = json.load(json_file)
+        if user_data.get("user_id") == callback.from_user.id:
+            user_data["flag"] = False
+            with open(user_file_path, 'w') as json_file:
+                json.dump(user_data, json_file)
+
+    await callback.message.answer('Прослушивание остановлено 🔐')
+
 
 
